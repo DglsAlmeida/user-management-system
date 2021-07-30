3d4b178159b4f7d51e6c9de1ce3ec15e8bfaa7cf
rivand <rivand@RIVAND1>
2021-07-29 21:27:40 -0300

    finish users page layout

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,24 +1,31 @@
 import { Input } from "../../components/Input";
-import { Button, Container, ImgContainer, SignInContainerContent, SignInForm, SignInPageContent, Title } from "./styles";
+import {
+  Button,
+  Container,
+  ImgContainer,
+  SignInContainerContent,
+  SignInForm,
+  SignInPageContent,
+  Title,
+} from "./styles";
 import teamPage from "../../assets/team_page.svg";
 
 export const SignIn = () => {
   return (
     <Container>
       <SignInContainerContent>
-      <ImgContainer>
-        <img src={teamPage} alt="team page logo" />
-      </ImgContainer>
-      <Title>Faça seu Login</Title>
-      <SignInPageContent>
-        <SignInForm>
-          <Input name="name" type="text" placeholder="Nome"/>
-          <Input name="email" type="email" placeholder="E-mail"/>
-          <Input name="password" type="password" placeholder="Senha"/>
-          <Button>Entrar</Button>
-        </SignInForm>
-      </SignInPageContent>
-
+        <ImgContainer>
+          <img src={teamPage} alt="team page logo" />
+        </ImgContainer>
+        <Title>Faça seu Login</Title>
+        <SignInPageContent>
+          <SignInForm>
+            <Input name="name" type="text" placeholder="Nome" />
+            <Input name="email" type="email" placeholder="E-mail" />
+            <Input name="password" type="password" placeholder="Senha" />
+            <Button>Entrar</Button>
+          </SignInForm>
+        </SignInPageContent>
       </SignInContainerContent>
     </Container>
   );
